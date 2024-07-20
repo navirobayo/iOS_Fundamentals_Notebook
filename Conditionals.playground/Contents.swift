365e43fd041ed0094f578fd98fc2e08b796a839d
navirobayo <navirobayo@gmail.com>
2024-07-20 11:18:58 -0500

    Dentist Coding 2

--- Conditionals.playground/Contents.swift
+++ Conditionals.playground/Contents.swift
@@ -1,3 +1,70 @@
-import UIKit
+// MARK: Conditionals.
 
-var greeting = "Hello, playground"
+// Logic Gates.
+
+/* Add notes here later on.*/
+
+var greeting = "Hello User"
+
+var x = 9
+var y = 3
+var inPain = true
+
+if x == y {
+    // if block.
+} else {
+    // else block.
+}
+
+inPain = false
+
+
+if x == y {
+    print("x is equal to y")
+} else {
+    print("x is not equal to y")
+}
+
+if inPain {
+    print("Go to the dentist.")
+} else {
+    print("Great! No pain no gain!")
+}
+
+// MARK: Dentist Excercise.
+
+var painLevel = 45
+var healthLevel = 89
+
+if healthLevel <= painLevel {
+    print("You are screwed dude")
+}
+else if healthLevel > painLevel {
+    print("Do not complain. You got this. ")
+}
+
+var height = 1600
+var dangerousAltitute = 500
+var ultraDangerousAltitude = 1500
+var acceptableAltitude = 10
+
+if height >= acceptableAltitude {
+    print("You need a harnes for that bro!1")
+} else if height >= ultraDangerousAltitude {
+    print("No way bro.")
+}
+if height >= acceptableAltitude {
+    print("You need a harnes for that bro!2")
+} else if height >= ultraDangerousAltitude {
+    print("No way bro. 2")
+}
+
+else if height >= dangerousAltitute {
+    print("You need a harnes for that bro!")
+} else if height >= ultraDangerousAltitude {
+    print("No way bro. Get down.")
+} else {
+    print("Unknown data")
+}
+
+
--- Reecap 002.playground/Contents.swift
+++ Reecap 002.playground/Contents.swift
@@ -1,3 +1,29 @@
-import UIKit
+var workerAltitude = 56
+var safeAltitude = 25
+var dangerousAltitude = 50
+var ultraDangerousAltitude = 75
 
-var greeting = "Hello, playground"
+if workerAltitude <= safeAltitude {
+    print("No worries bro! Climb that altitude!")
+} else if workerAltitude <= dangerousAltitude {
+    print("You need a harnes for that bro!")
+} else {
+    print("What are you even doing bro. ")
+}
+
+var cashInBank = 34000000
+var priceOfMotorbike = 19000000
+var interestRate = 0.9
+
+if cashInBank >= priceOfMotorbike || interestRate == 0.6 {
+    print("Go for it bro. Get that motorbike")
+} else {
+    print("You gotta save some cash bro")
+}
+
+if cashInBank >= priceOfMotorbike && interestRate == 0.6 {
+    print("Go for it bro. Get that motorbike")
+} else {
+    print("You gotta save some cash bro")
+}
+
