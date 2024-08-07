39eb09dc2fbb8b8e4358ae8fe497608b2fc94418
navirobayo <navirobayo@gmail.com>
2024-08-07 16:53:19 -0500

    Functions study + reecap

---
+++ Functions.playground/Contents.swift
@@ -0,0 +1,48 @@
+// MARK: Functions.
+
+// Reusable pieces of code.
+
+func greetFunction() {
+    print("hello bro, nice function.")
+}
+
+greetFunction()
+
+func greetFunctionPrime() {
+    print("You actually know how to program. It is just about using it properly, without looking everysingle step on the internet. Dart gave you many of the fundamentals now that I see this")
+}
+
+greetFunctionPrime()
+
+// The equal sign will assign a value. The colon specifies the type of value.
+
+func greetUser(userName: String) {
+    print("Hey \(userName), are you programming or what")
+}
+
+greetUser(userName: "Ivan")
+
+func attitudeFunction() {
+    print("Yes sr. You bet I am.")
+}
+
+attitudeFunction()
+
+
+func numericalFunction(kilometers: Int, fat: Int) {
+    print("If you run \(kilometers) kilometers, you will burn \(fat) grams of fat. That means my friend you have currently burned \(kilometers / fat) calories.")
+}
+
+func caloriesFunction(kilometers: Int, fat: Int) -> Int {
+    return kilometers / fat
+   
+}
+
+numericalFunction(kilometers: 10, fat: 2)
+
+let caloriesBurned = caloriesFunction(kilometers: 9, fat: 3)
+print ("You my friend, have just burned \(caloriesBurned) calories.")
+
+// MARK: Return a value.
+
+
