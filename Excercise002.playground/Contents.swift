37a87da6978f095d2dc85602580bbe47a0e98b7f
navirobayo <navirobayo@gmail.com>
2024-08-11 12:58:00 -0500

    Excercise 002

--- Excercise002.playground/Contents.swift
+++ Excercise002.playground/Contents.swift
@@ -4,8 +4,47 @@
 // - Min value
 
 var arrayOfNumbers = [6, 3, 8, 6, 9, 0, 8, 6, 7, 4, 23, 124, 54, 5]
-
+var totalSum = 0
+print("Total Sum for now: \(totalSum)")
 func calculationOne() {
-    
-    return
+    for i in arrayOfNumbers {
+        totalSum += i
+    }
 }
+calculationOne()
+
+print("Total Sum after the calculation: \(totalSum)")
+
+print("------------------------------")
+
+var maximumValue = 0
+
+print("The maximum value so far is: \(maximumValue)")
+
+func calculationTwo() {
+    for i in arrayOfNumbers {
+        if i > maximumValue {
+            maximumValue = i
+        }
+    }
+}
+
+calculationTwo()
+print("The maximum value after the calculation is: \(maximumValue)")
+
+print("------------------------------")
+var minimumValue = maximumValue
+print("The minimum value so far is \(minimumValue)")
+
+func calculationThree() {
+    for i in arrayOfNumbers {
+        if i < minimumValue {
+            minimumValue = i
+        }
+    }
+}
+calculationThree()
+
+print("The minimum value after the calculation is: \(minimumValue)")
+    
+    
